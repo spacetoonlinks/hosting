83c6af8160b002c4fb7dc2da1964ebccbd243e1b
spacetoonlinks <111027023+spacetoonlinks@users.noreply.github.com>
2023-12-28 05:12:27 +0300

    popup-modal.js

--- popup-modal.js
+++ popup-modal.js
@@ -1,13 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Get the button element by its ID
     var watchAdButton = document.getElementById('watchAdButton');
-
-    // Add a click event listener to the button
     watchAdButton.addEventListener('click', function () {
-        // Get the modal element by its ID
         var modal = document.getElementById('modal');
-
-        // Hide the modal by setting its display property to 'none'
         modal.style.display = 'none';
     });
 });
